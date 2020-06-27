296098eef10a567b4900a05d6e5a50e2c65c5829
Dominique LaSalle <dominique@solidlake.com>
2020-06-27 12:31:39 -0700

    Fix test warnings

--- test/buffer_clearreset_test.c
+++ test/buffer_clearreset_test.c
@@ -8,29 +8,27 @@
 
 sint_t test(void) 
 {
-  sint_t i,j;
+  sint_buffer_t * const buffer = sint_buffer_create(M);
 
-  sint_buffer_t * buffer = sint_buffer_create(M);
-
-  TESTEQUALS(0UL,buffer->size,PF_SIZE_T);
+  TESTEQUALS((size_t)0,buffer->size,PF_SIZE_T);
 
   buffer->elements = sint_realloc(buffer->elements,N);
   buffer->maxsize = N;
   buffer->size = N;
-  sint_incset(buffer->elements,N);
+  sint_incset(buffer->elements,0, 1, N);
 
   sint_buffer_clear(buffer);
 
-  TESTEQUALS(0,buffer->size,PF_SIZE_T);
-  TESTEQUALS(N,buffer->maxsize,PF_SIZE_T);
+  TESTEQUALS((size_t)0,buffer->size,PF_SIZE_T);
+  TESTEQUALS((size_t)N,buffer->maxsize,PF_SIZE_T);
 
-  sint_incset(buffer->elements,M);
+  sint_incset(buffer->elements,0,1,M);
   buffer->size = M;
 
   sint_buffer_reset(buffer);
 
-  TESTEQUALS(0,buffer->size,PF_SIZE_T);
-  TESTEQUALS(M,buffer->maxsize,PF_SIZE_T);
+  TESTEQUALS((size_t)0,buffer->size,PF_SIZE_T);
+  TESTEQUALS((size_t)M,buffer->maxsize,PF_SIZE_T);
 
   sint_buffer_free(buffer);
 
--- test/list_linked_create_test.c
+++ test/list_linked_create_test.c
@@ -5,8 +5,8 @@ sint_t test(void)
 {
   sint_list_t * list = sint_list_create();
 
-  TESTEQUALS(list->head,NULL,"%p");
-  TESTEQUALS(list->tail,NULL,"%p");
+  TESTEQUALS((const void*)list->head,NULL,"%p");
+  TESTEQUALS((const void*)list->tail,NULL,"%p");
   TESTEQUALS(list->size,0UL,"%zu");
 
   sint_list_free(list);
